278426221e02c30e5909b95a7420652a520bb11e
Арина <107919530+Tudaruna@users.noreply.github.com>
2023-12-10 17:49:39 +0300

    Delete client directory

--- client/js/intro.js
+++
@@ -1,6 +0,0 @@
-function smoothPageTransition() {
-  document.body.classList.add('transition-out');
-  setTimeout(function() {
-    window.location.href = "About.html";
-  }, 1000); // Задержка перед переходом, здесь 1000 миллисекунд (1 секунда)
-}
--- client/js/menu.js
+++
@@ -1,9 +0,0 @@
-const menuButton = document.getElementById("menuButton");
-const menu = document.getElementById("menu");
-
-// Обработчик клика по кнопке
-menuButton.addEventListener("click", function () {
-  // Переключение класса 'active' при клике
-  console.log("Work");
-  menu.classList.toggle("active");
-});
